28033141285e0e7d155128c4540c2529399921a5
meynm <meynmd@users.noreply.gibhub.com>
2016-04-12 15:26:10 -0700

    more documentation

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,31 @@
 
 int main() {
 	// testing some data structures
+	// to create a Game object, you must first create a PlayerCharacter and a GameMap object
 	PlayerCharacter* pc = new PlayerCharacter(string("hero"), 100, 200);
+	GameMap* gm = new GameMap(Coord(7, 5));
+
+	// Let's also create another character
 	Character* c = new Character(1, string("enemy"), 50, 0);
 	c->symbol = 'E';
+
+	// Create a room to put into the map
 	Room* r = new Room(3, Coord(5, 6), Coord(2, 3), string("my room"),
-					string("You find yourself in the middle of a large field surrounded by hills. To the north and east is a dense forest. An enemy lurks to the west."));
-	GameMap* gm = new GameMap(Coord(7, 5));
+			string(
+					"You find yourself in the middle of a large field surrounded by hills. "
+					"To the north and east is a dense forest. An enemy lurks to the west."
+					) );
+	// place our "enemy" character into our room
 	r->insertAt(Coord(0, 4), c);
 
+	// place our room into the game map
 	gm->insertRoom(r, Coord(2, 1));
 	gm->setStartRoom(Coord(2, 1));
 
+	// instantiate a Game object with the player character and game map
 	Game* g = new Game(pc, gm);
+
+	// print a representation of the game map and room we've created
 	gm->print();
 	printf("\n\n\n");
 	r->print();
